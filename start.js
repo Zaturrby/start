735409a38b9e1d6a7640115a5c71ffed127875c5
Zaturrby <rjkorteschiel@gmail.com>
2014-09-03 16:23:58 +0200

    assignment for Meteor

--- start.js
+++ start.js
@@ -1,29 +1,49 @@
-Counter = new Meteor.Collection('counter');
+Names = new Meteor.Collection('names');
 
 if (Meteor.isClient) {
-  
-  Meteor.call('getGists', function(error, result){
-    Session.set('gists', result);
-  });
 
   Template.hello.helpers({
-    counter: function () {
-      if(Meteor.user()){
+    names: function(){
+      var namesList = ["Loading usernames"];
+      
+      if (Meteor.user()){
         var user = Meteor.user().services.github.username;
-        var myCounter = Counter.findOne({'name': user});
-        if(!myCounter){
-          myCounter = Counter.insert({'name': user, 'count': 0});
+
+        if(Names.findOne({'name': user})) {
+          console.log("user already in the MiniMongoNamesDB");
+        } else {
+          Names.insert({'name': user});
+          console.log("new user inserted in the DB");
         }
-        return "You have clicked " + myCounter.count + " times";
-      } else {
-        return "not available";
       }
+
+      var namesList = Names.find({}).fetch().map(function(singleName){
+        return singleName.name;
+      });
+
+      return namesList;
     },
-    name: function(){
-      return Meteor.user().services.github.username;
-    },
-    gists: function(){
-      return Session.get('gists'); 
+    gists: function () {
+      if(Session.get('nameSelected')){
+
+        // calling serverside Gistfunction
+        Meteor.call('getGists', Session.get('nameSelected'), function(error, result){
+         Session.set('gists', result);
+        });
+
+        // setting return values
+        var noGists = [ { "url": ["Gists are loading or user has no gists"] } ];
+        
+        if(Session.get('gists')){
+          return Session.get('gists');
+        } else {
+          return noGists;
+        } 
+
+      } else {
+        var noUser = [ { "url": ["Select username to see gists"] } ];
+        return noUser;
+      } 
     }
   });
 
@@ -32,20 +52,27 @@ if (Meteor.isClient) {
       var user = Meteor.user().services.github.username;
       var myCounter = Counter.findOne({'name': user});
       Counter.update({_id: myCounter._id}, {$inc: {count: 1}});
+    },
+    'click li a': function (evnt) {
+      var selectedName = evnt.target.innerHTML;
+      Session.set('nameSelected', selectedName);
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.methods({
-    getGists: function(){
+    getNames: function() {
+
+    },
+    getGists: function(user){
       var GithubApi = Meteor.npmRequire('github');
       var github = new GithubApi({
         version: "3.0.0"
       });
 
       var gists = Async.runSync(function(done){
-        github.gists.getFromUser({user: 'yeehaa123'}, function(err, data){
+        github.gists.getFromUser({"user": user}, function(err, data){
           done(null, data);
         });
       });
