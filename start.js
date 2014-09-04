c39b402f0ade4591202d13a5a264e946203359da
Zaturrby <rjkorteschiel@gmail.com>
2014-09-04 22:51:46 +0200

    small changes

--- start.js
+++ start.js
@@ -10,10 +10,10 @@ if (Meteor.isClient) {
         var user = Meteor.user().services.github.username;
 
         if(Names.findOne({'name': user})) {
-          console.log("user already in the MiniMongoNamesDB");
+          console.log("User already in the DB");
         } else {
           Names.insert({'name': user});
-          console.log("new user inserted in the DB");
+          console.log("User stored in the DB");
         }
       }
 
@@ -26,33 +26,26 @@ if (Meteor.isClient) {
     gists: function () {
       if(Session.get('nameSelected')){
 
-        // calling serverside Gistfunction
         Meteor.call('getGists', Session.get('nameSelected'), function(error, result){
          Session.set('gists', result);
         });
-
-        // setting return values
-        var noGists = [ { "url": ["Gists are loading or user has no gists"] } ];
         
-        if(Session.get('gists')){
-          return Session.get('gists');
+        if (Session.get('gists')) {
+          if(Session.get('gists').length != 0){
+            return Session.get('gists');
+          } else {
+            return [{ "url": ["User has no gists."]}];
+          } 
         } else {
-          return noGists;
-        } 
-
+          return [{"url": ["Loading gists!"]}];
+        }
       } else {
-        var noUser = [ { "url": ["Select username to see gists"] } ];
-        return noUser;
+        return [{ "url": ["Select username to see gists"]}];
       } 
     }
   });
 
   Template.hello.events({
-    'click button': function () {
-      var user = Meteor.user().services.github.username;
-      var myCounter = Counter.findOne({'name': user});
-      Counter.update({_id: myCounter._id}, {$inc: {count: 1}});
-    },
     'click li a': function (evnt) {
       var selectedName = evnt.target.innerHTML;
       Session.set('nameSelected', selectedName);
@@ -62,9 +55,6 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.methods({
-    getNames: function() {
-
-    },
     getGists: function(user){
       var GithubApi = Meteor.npmRequire('github');
       var github = new GithubApi({
@@ -72,12 +62,23 @@ if (Meteor.isServer) {
       });
 
       var gists = Async.runSync(function(done){
+        github.authenticate({
+          type: "basic",
+          username: "ZaturrbyTester",
+          password: "SolidSnake25"
+        });
+
         github.gists.getFromUser({"user": user}, function(err, data){
           done(null, data);
         });
+
       });
 
-      return gists.result;
+      if (gists.result) {
+        return gists.result;
+       } else {
+        return [{"url": ["Error retrieving the gists from github"]}];
+       }
     }
   });
   Meteor.startup(function () {
